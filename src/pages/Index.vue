3272fb80ce31a5055535453fb344062dcb3af39f
YudzhinNSK <spidi2e@mail.ru>
2022-02-10 12:05:37 +0300

    Create simple App

---
+++ src/pages/Index.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <button @click="buttonClick">
+      Текст кнопки
+    </button>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent} from 'vue';
+
+export default defineComponent({
+  name: 'PageIndex',
+  methods:{
+    buttonClick(){
+      console.log('click');
+      alert('click');
+    }
+  },
+
+});
+</script>
